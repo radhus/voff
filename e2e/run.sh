6a699430d845a1592567484a3cc4efe8730fe74c
William Johansson <radar@radhuset.org>
2020-05-13 23:42:09 +0200

    Run E2E tests on PRs

--- e2e/run.sh
+++ e2e/run.sh
@@ -14,10 +14,10 @@ set +e
 exitcode=0
 
 ./run.exp 5seconds.yaml
-[ $? -ne 0 ] && "echo ::error::5 seconds test failed" && exitcode=1
+[ $? -ne 0 ] && echo "::error::5 seconds test failed" && exitcode=1
 
 ./run.exp forever.yaml
-[ $? -ne 4 ] && "echo ::error::Forever timeout test failed" && exitcode=1
+[ $? -ne 4 ] && echo "::error::Forever timeout test failed" && exitcode=1
 
 echo "Test suite complete"
 exit $exitcode
